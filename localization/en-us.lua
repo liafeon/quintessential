bfc5d3ac4a2c2cf3987a58a15c7ddad7c4a39c4d
Amy Hurrell <taliajhurrell@gmail.com>
2025-03-19 21:29:37 +0000

    Elizabeth Tower
    Straights may contain cards both higher and lower than an Ace

---
+++ localization/en-us.lua
@@ -0,0 +1,13 @@
+return {
+    descriptions = {
+        Joker = {
+            j_quint_elizabethtower = {
+                name = "Elizabeth Tower",
+                text = {
+                    "{C:attention}Straights{} may contain cards",
+                    "both higher and lower than an {C:attention}Ace{}"
+                },
+            },
+        },
+    },
+}
---
+++ src/jokers/elizabethtower.lua
@@ -0,0 +1,16 @@
+SMODS.Joker { 
+    key = "elizabethtower",
+    config = {},
+    loc_vars = function(self, info_queue, card)
+        return {}
+    end,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    rarity = 2,
+    atlas = "atlas",
+    pos = {x = 0, y = 0},
+    cost = 5
+}
