2670c8d181d882dbfb8ed351276ceb6f12520d7f
Amy Hurrell <taliajhurrell@gmail.com>
2026-01-12 20:36:26 +0000

    jokers en-us localisation

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -11,7 +11,7 @@ return {
             },
             j_quint_threeflavours = {
                 name = "Three Flavours",
-                text={
+                text= {
                     "{C:chips}+#1#{} Chips",
                     "Eat a cone if played",
                     "hand contains",
@@ -20,7 +20,7 @@ return {
             },
             j_quint_landsend = {
                 name = "Land's End",
-                text={
+                text= {
                     "{X:mult,C:white}X#2#{} Mult for each",
                     "copy of your most",
                     "common playing card",
@@ -28,6 +28,98 @@ return {
                     "{C:inactive}(Currently {X:mult,C:white}X#1#{} {C:inactive}Mult)"
                 },
             },
+            j_quint_teabag = {
+                name = "Teabag",
+                text = {
+                    "In {C:attention}#1#{} rounds",
+                    "Doubles money",
+                    "{C:inactive}(Max of {C:money}$#2#{C:inactive})"
+                }
+            },
+            j_quint_thetube = {
+                name = "The Tube",
+                text = {
+                    "This joker gains {C:chips}+#1#{} Chips",
+                    "if played hand contains only",
+                    "{C:hearts}Hearts{} and {C:diamonds}Diamonds",
+                    "{C:inactive}(Currently {C:chips}+#1#{}+#2# {C:inactive}Chips)"
+                }
+            },
+            j_quint_rose = {
+                name = "Rose",
+                text = {
+                    "{C:attention}British Cards{} gain",
+                    "{C:mult}+#1#{} Mult when scored"
+                }
+            },
+            j_quint_clover = {
+                name = "Clover",
+                text = {
+                    "{C:attention}British Cards{} gain",
+                    "{C:money}$#1#{} when scored"
+                }
+            },
+            j_quint_thistle = {
+                name = "Rose",
+                text = {
+                    "{C:attention}British Cards{} gain",
+                    "{C:chips}+#1#{} Chips when scored"
+                }
+            },
+            j_quint_daffodil = {
+                name = "Rose",
+                text = {
+                    "{C:attention}British Cards{} gain",
+                    "{X:mult,C:white}X#1#{} Mult when scored"
+                }
+            },
+            j_quint_hogmanay = {
+                name = "Hogmanay",
+                text = {
+                    "This joker gains {X:mult,C:white}X#1#{} Mult",
+                    "after defeating any blind",
+                    "Resets when {C:attention}Boss Blind{} is defeated",
+                    "{C:inactive}(Currently {X:mult,C:white}X#2#{} {C:inactive}Mult)"
+                }
+            },
+            j_quint_licensetokill = {
+                name = "License to Kill",
+                text = {
+                    "Retrigger played {C:attention}#1#{}s",
+                    "{C:attention}#2#{} additional times"
+                }
+            },
+            j_quint_newpoundcoin = {
+                name = "New Pound Coin",
+                text = {
+                    "Halves all {C:attention}listed",
+                    "{C:green,E:1,S:1.1}probabilities",
+                    "{C:inactive}(ex: {C:green}1 in 3{C:inactive} -> {C:green}1 in 6{C:inactive})",
+                }
+            },
+            j_quint_angel = {
+                name = "Angel of the North",
+                text = {
+                    "Gains {C:money}$#1#{} sell value",
+                    "every time a {C:tarot}Tarot{} card is used",
+                }
+            },
+            j_quint_timemachine = {
+                name = "Time Machine",
+                text = {
+                    "If {C:attention}first discard{} of round",
+                    "has only {C:attention}#1#{} card, add a",
+                    "permanent copy to deck",
+                    "and draw it to {C:attention}hand"
+                }
+            },
+            j_quint_passport = {
+                name = "Passport",
+                text = {
+                    "All played unenhanced {C:attention}face cards{}",
+                    "become {C:attention}British{} cards when scored"
+                }
+            }
         },
     },
 }
